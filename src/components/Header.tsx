5a944dc6304339c24cd86021dfab5cd8f866f7f8
chenhaojie9527 <a18520145493@163.com>
2023-05-02 21:38:22 +0800

    feat: header 登录与注册

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,10 +5,26 @@ interface HeaderProps {
   size: number;
 }
 
+function HeaderIcon() {
+  return (
+    <div className="headerIcon pr-2 pl-2">
+      <div className="">
+        <span className="cursor-pointer hover:text-[pink] text-[red]">
+          登录
+        </span>
+        <span className="pr-1 pl-1 text-[red]">|</span>
+        <span className="cursor-pointer text-[red] hover:text-[pink]">
+          注册
+        </span>
+      </div>
+    </div>
+  );
+}
+
 const Header: FC<HeaderProps> = ({ content, size }) => {
   return (
-    <div className="border-solid border-[#eee] border-b-[1px] h-[40px]">
-      Header
+    <div className="border-solid border-[#eee] border-b-[1px] h-[40px] flex items-center">
+      <HeaderIcon />
     </div>
   );
 };
